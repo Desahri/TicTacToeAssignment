c6fac8f4614716b9d8a6c23d2eb9eb29c347a4cb
desahri <didier96@live.nl>
2019-02-25 17:38:01 +0100

    Game now allows X and O to be placed after each other

--- my-app/src/tictactoeField/field.js
+++ my-app/src/tictactoeField/field.js
@@ -8,37 +8,59 @@ class Field extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            gridValues: [
-                '.', '.', '.', 
-                '.', '.', '.',
-                '.', '.', '.', 
-            ]
+            gridValues: Array(9).fill('.'),
+            xTurn: true,
         }
     }
+
+    /*
+     * returns the grid with the relevant value ('.', 'X' or 'O')
+     */
     getGrid(gridNr) {
         return <Grid value={this.state.gridValues[gridNr]} onClick={() => this.gridClick(gridNr)} />
     }
+
+    /*
+     * Action to be performed after a click
+     */
     gridClick(gridNr) {
+        //get the current grid values
         var newGrids = this.state.gridValues.slice();
-        newGrids[gridNr] = 'O';
+
+        //Don't do anything if the grid is already used
+        if(newGrids[gridNr] != '.') {
+            return;
+        }
+
+        //set the value of the grid depending on who's turn it was
+        newGrids[gridNr] = this.state.xTurn ? 'X' : 'O';
         this.setState({
-            gridValues: newGrids 
+            gridValues: newGrids,
+            xTurn: !this.state.xTurn,
         });
     }
+
     render() {
+        var turn = this.state.xTurn ? " turnX" : " turnO";
+
         return(
             <div className='field'>
-                {this.getGrid(0)}
-                {this.getGrid(1)}
-                {this.getGrid(2)}
-                <br></br>
-                {this.getGrid(3)}
-                {this.getGrid(4)}
-                {this.getGrid(5)}
-                <br></br>
-                {this.getGrid(6)}
-                {this.getGrid(7)}
-                {this.getGrid(8)}
+                <div className="fieldRow">
+                    {this.getGrid(0)}
+                    {this.getGrid(1)}
+                    {this.getGrid(2)}
+                </div>
+                <div className="fieldRow">
+                    {this.getGrid(3)}
+                    {this.getGrid(4)}
+                    {this.getGrid(5)}
+                </div>
+                <div className="fieldRow">
+                    {this.getGrid(6)}
+                    {this.getGrid(7)}
+                    {this.getGrid(8)}
+                </div>
+                <div className={"fieldRow" + turn} />
             </div>
         );
     }
--- my-app/src/tictactoeField/grid.js
+++ my-app/src/tictactoeField/grid.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 import './grid.css';
 
+
+
 function Grid(props) {
+    let col = "";
+    switch(props.value) {
+        case 'X':
+        col = " gridX";
+        break;
+        case 'O':
+        col = " gridO";
+        break;
+    }
     return(
-        <button className="grid" onClick={props.onClick}>
+        <button className={"grid" + col} onClick={props.onClick}>
             {props.value}
         </button>
     );
